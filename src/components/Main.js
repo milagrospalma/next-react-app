dd2e777ebaed9dcce4cdc2b638ff28fac7713a86
milagrospalma <mnpalma.2@gmail.com>
2019-04-10 18:43:57 -0500

    [EventLab]: add slider

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import SimpleSlider from './SimpleSlider';
 
 const Main = () => (
     <main>
-        <p>hey</p>
+        <h2>Bienvenido</h2>
+        <p>
+            Nostrud non fugiat commodo excepteur velit proident pariatur excepteur irure aute enim. Voluptate sunt cillum adipisicing sint nisi nostrud reprehenderit. Dolor reprehenderit minim ad et exercitation esse sit ut reprehenderit. Ipsum ea tempor eiusmod consectetur sit cillum enim qui culpa Lorem ad mollit sit. Elit ex aliquip officia anim veniam ipsum laborum aute veniam.
+        </p>
+        <SimpleSlider />
     </main>
 )
-
 export default Main
---
+++ src/components/SimpleSlider.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react"
+import Slider from "react-slick"
+import './_slider.scss'
+
+class SimpleSlider extends Component {
+    render() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            fade: true,
+            cssEase: 'linear'
+        };
+        return (
+            <div className="slick-container">
+                <Slider {...settings}>
+                    <div>
+                        <h3>1</h3>
+                    </div>
+                    <div>
+                        <h3>2</h3>
+                    </div>
+                    <div>
+                        <h3>3</h3>
+                    </div>
+                </Slider>
+            </div>
+        );
+    }
+}
+
+export default SimpleSlider
